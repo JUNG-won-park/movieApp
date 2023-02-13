c1269eb28e939b149da8610f01437e5b22c9f1e0
JUNG-won-park <ahg0525@gmail.com>
2023-02-14 03:17:25 +0900

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'fb5f416d1716f9f197604033ff68ffdc';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
